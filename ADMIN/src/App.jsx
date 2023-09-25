e690f242555652c25a9e7ddfd66a988bf80985c1
bichnhung <100755405+bichnhung@users.noreply.github.com>
2023-09-25 16:45:14 +0700

    Update_FormLogin

--- ADMIN/src/App.jsx
+++ ADMIN/src/App.jsx
@@ -1,15 +1,15 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Login from './components/Login';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Login from "./components/Login";
 function App() {
   return (
     <>
       <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </Router>
+        <Navbar />
+        <Routes>
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Router>
     </>
   );
 }
--- ADMIN/src/components/Login.jsx
+++ ADMIN/src/components/Login.jsx
@@ -1,40 +1,22 @@
-
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
-import Container from '@mui/material/Container';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import Typography from '@mui/material/Typography';
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
 function Login() {
-    return (
-      <div style={{ display: 'flex', height: '100vh', justifyContent: 'center', alignItems: 'center' }}>
+  return (
+    <div style={{ display: "flex", height: "100vh", justifyContent: "center", alignItems: "center" }}>
       <Container maxWidth="sm">
         <Card>
           <CardContent>
-            <Typography variant="h5" style={{ textAlign: 'center', marginBottom: '1rem' }}>Đăng nhập</Typography>
+            <Typography variant="h5" style={{ textAlign: "center", marginBottom: "1rem" }}>
+              Đăng nhập
+            </Typography>
             <form>
-              <TextField 
-                fullWidth 
-                variant="outlined" 
-                label="Tên đăng nhập" 
-                margin="normal"
-                required
-              />
-              <TextField 
-                fullWidth 
-                variant="outlined" 
-                label="Mật khẩu"
-                type="password"
-                margin="normal"
-                required
-              />
-              <Button 
-                variant="contained" 
-                color="primary" 
-                type="submit" 
-                fullWidth
-                style={{ marginTop: '1rem' }}
-              >
+              <TextField fullWidth variant="outlined" label="Tên đăng nhập" margin="normal" required />
+              <TextField fullWidth variant="outlined" label="Mật khẩu" type="password" margin="normal" required />
+              <Button variant="contained" color="primary" type="submit" fullWidth style={{ marginTop: "1rem" }}>
                 Đăng nhập
               </Button>
             </form>
@@ -42,7 +24,6 @@ function Login() {
         </Card>
       </Container>
     </div>
-    
-    );
-  }
-  export default Login;
+  );
+}
+export default Login;
