0db06a46840344eed96926b75405b7de2be43f2e
chauvanhieu <chauhieu.nina@gmail.com>
2023-09-29 05:17:28 +0700

    update upload service and setup ckeditor

--- ADMIN/src/services/upload.service.js
+++ ADMIN/src/services/upload.service.js
@@ -9,18 +9,24 @@ const UploadService = {
       formData.append("file", file);
       const token = localStorage.getItem("access_token");
 
-      const response = await axios.post(`${baseURL}single`, formData, {
-        headers: {
-          "Content-Type": "multipart/form-data",
-          Authorization: `Bearer ${token}`,
-        },
-      });
+      const response = await axios.post(
+        `${baseURL}api/upload/single`,
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
 
       if (!response.data.filePath) {
         throw new Error("Failed to upload file");
       }
 
-      return response.data.filePath;
+      const filepath = `${baseURL.slice(0, -1)}${response.data.filePath}`;
+
+      return filepath;
     } catch (error) {
       console.error("Error uploading single file:", error);
       throw error;
@@ -32,19 +38,26 @@ const UploadService = {
       const formData = new FormData();
       files.forEach((file) => formData.append("files", file));
       const token = localStorage.getItem("access_token");
-
-      const response = await axios.post(`${baseURL}multiple`, formData, {
-        headers: {
-          "Content-Type": "multipart/form-data",
-          Authorization: `Bearer ${token}`,
-        },
-      });
+      const response = await axios.post(
+        `${baseURL}api/upload/multiple`,
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
 
       if (!response.data.filePaths) {
         throw new Error("Failed to upload files");
       }
 
-      return response.data.filePaths;
+      const filePaths = response.data.filePaths.map(
+        (filePath) => `${baseURL.slice(0, -1)}${filePath}`
+      );
+
+      return filePaths;
     } catch (error) {
       console.error("Error uploading multiple files:", error);
       throw error;
