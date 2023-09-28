fd01047b40f8573b50820f35a6da0d55ab01aa72
chauvanhieu <chauhieu.nina@gmail.com>
2023-09-29 04:34:48 +0700

    update userSlice

--- ADMIN/src/redux/store.js
+++ ADMIN/src/redux/store.js
@@ -3,10 +3,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authSlice from "./slices/auth.slice";
 import industriesSlice from "./slices/industries.slice";
+import userSlice from "./slices/user.slice";
 
 export default configureStore({
   reducer: {
     authentication: authSlice,
+    user: userSlice,
     industries: industriesSlice,
   },
 });
