b7661b331caa670a334e5758c1a6bdd64f3d9245
Cherryce <mrpii.2k3@gmailcom>
2023-09-29 08:36:41 +0700

    Uploading

--- SERVER/src/routes/auth.route.js
+++ SERVER/src/routes/auth.route.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const passport = require("passport");
 const router = express.Router();
 const adminAuthController = require("../controller/auth/adminAuth.controller");
 const customerController = require("../controller/auth/customerAuth.controller");
+const googleAuthController = require("../controller/auth/googleAuth.controller");
 const shopAuthController = require("../controller/auth/shopAuth.controller");
 
 router.post("/admin/login", adminAuthController.login);
@@ -14,3 +16,28 @@ router.post("/customer/login", customerController.login);
 router.post("/customer/register", customerController.register);
 
 module.exports = router;
+
+router.get(
+  "/google",
+  passport.authenticate("google", { scope: ["email", "profile"] })
+);
+
+router.get(
+  "/google/callback",
+  passport.authenticate("google", {
+    successRedirect: "/auth/google/success",
+    failureRedirect: "/auth/google/failure",
+  })
+);
+
+router.use(
+  passport.session({
+    secret: "mysecret",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false },
+  })
+);
+
+router.use(passport.initialize());
+router.use(passport.session());
